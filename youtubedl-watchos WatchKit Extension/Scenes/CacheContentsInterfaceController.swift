59741f85d54bb5b2463d375852894d9680baf19f
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-12-07 17:14:34 +0000

    idk

---
+++ youtubedl-watchos WatchKit Extension/Scenes/CacheContentsInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  CacheContentsInterfaceController.swift
+//  youtubedl-watchos WatchKit Extension
+//
+//  Created by llsc12 on 07/12/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class CacheContentsInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
