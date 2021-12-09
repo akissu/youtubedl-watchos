c84425b24cc65df3694dd1cc6c7a2d0b7ef1977d
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-12-09 20:45:26 +0000

    temporary icon

--- youtubedl-watchos WatchKit Extension/Scenes/CacheContentsInterfaceController.swift
+++ youtubedl-watchos WatchKit Extension/Scenes/CacheContentsInterfaceController.swift
@@ -10,7 +10,12 @@ import Foundation
 
 
 class CacheContentsInterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var videoTableRow: WKInterfaceTable!
+    @IBOutlet weak var cacheTitle: WKInterfaceLabel!
+    @IBOutlet weak var cacheImg: WKInterfaceImage!
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -19,15 +24,33 @@ class CacheContentsInterfaceController: WKInterfaceController {
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
-        
+        /*
         //load videos with accompanying metadata.
         do {
             let files = try FileManager.default.contentsOfDirectory(atPath: NSHomeDirectory()+"/Documents/cache")
+            for file in files {
+                videoTableRow.setNumberOfRows(files.count, withRowType: "VideoRow")
+                
+                for i in 0 ..< files.count {
+                    guard let row = videoTableRow.rowController(at: i) as? VideoRow else {
+                        continue
+                        }
+                    cacheTitle.titleLabel.setText(videos[i].title)
+                    row.videoId = videos[i].id
+                    
+                    if UserDefaults.standard.bool(forKey: settingsKeys.thumbnailsToggle) == false {
+                        row.thumbImg.setHidden(true)
+                    } else {
+                        row.thumbImg.sd_setImage(with: videos[i].img)
+                    }
+                }
+            }
             
         } catch {
             //no errors should occur i hope
             print(error)
         }
+         */
         super.willActivate()
     }
 
---
+++ youtubedl-watchos WatchKit Extension/Views/CacheTableRow.swift
@@ -0,0 +1,13 @@
+//
+//  CacheTableRow.swift
+//  youtubedl-watchos WatchKit Extension
+//
+//  Created by llsc12 on 09/12/2021.
+//
+
+import WatchKit
+import Foundation
+
+class CacheTableRow: NSObject {
+
+}
