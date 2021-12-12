1a20d142da6cb11758ea7c205957bc0d5bbaa01b
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-12-12 23:53:20 +0000

    change icon slightly

--- youtubedl-watchos WatchKit Extension/Scenes/InfoInterfaceController.swift
+++ youtubedl-watchos WatchKit Extension/Scenes/InfoInterfaceController.swift
@@ -8,14 +8,19 @@
 import WatchKit
 import Foundation
 import Alamofire
+import SwiftyJSON
 
-class CacheInfoInterfaceController: WKInterfaceController {
-
+class InfoInterfaceController: WKInterfaceController {
+    @IBOutlet weak var viewsLabel: WKInterfaceLabel!
+    @IBOutlet weak var likesLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        AF.request("https://"+Constants.downloadSrvInstance+"/api/v1/getInfo?url=\(context!)").responseJSON { response in
+        //AF.request("https://"+Constants.downloadSrvInstance+"/api/v1/getInfo?url=\(context!)").responseJSON { response in
             
-        }
+            //self.viewsLabel.setText("\(views!) Views")
+            //self.likesLabel.setText("\(likes!) Likes")
+        //}
 
         
         // Configure interface objects here.
